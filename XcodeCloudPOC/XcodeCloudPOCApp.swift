7c1553bd500cee3eac3a54a1d73d9171d079be76
AbdelahmanZimozi <46462169+AbdelrahmanAhmedShawky@users.noreply.github.com>
2024-03-15 05:55:42 +0200

    first commit

---
+++ XcodeCloudPOC/XcodeCloudPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudPOCApp.swift
+//  XcodeCloudPOC
+//
+//  Created by AbdelRahman Ahmed on 15/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
